9fdf033255e1f531c46f5449dbff48bb0c807484
mohi010 <mohitgaming8595@gmail.com>
2026-06-23 20:18:23 +0530

    Time: 0 ms (100%), Space: 42.9 MB (61.04%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,46 +1,59 @@
 class Solution {
     public boolean check(int[] arr) {
-        int n = arr.length;
-        int idx = -1;
-        for (int i =0 ; i<n-1 ; i++){
-            if (arr[i+1]<arr[i]){
-                idx= i;
-                break;
-            }
-        }
-        if (idx==-1) return true;
-        int i = 0;
-        int j = idx;
-        while (i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
 
-        i= idx+1;
-        j = n-1;
-        while (i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+
+        // most optimaall-------
+
+        // arr[i] >arr[(i+1)%n]   n = arr.length;
+        int cnt =0;
+        int n = arr.length;
+        for (int i = 0 ; i<n; i++){
+            if (arr[i] >arr[(i+1)%n]) cnt++;
         }
-        i=0;
-        j=n-1;
-        while (i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
+        if (cnt<=1) return true;
+        else return false;
+
+        // int n = arr.length;
+        // int idx = -1;
+        // for (int i =0 ; i<n-1 ; i++){
+        //     if (arr[i+1]<arr[i]){
+        //         idx= i;
+        //         break;
+        //     }
+        // }
+        // if (idx==-1) return true;
+        // int i = 0;
+        // int j = idx;
+        // while (i<j){
+        //     int temp = arr[i];
+        //     arr[i] = arr[j];
+        //     arr[j] = temp;
+        //     i++;
+        //     j--;
+        // }
+
+        // i= idx+1;
+        // j = n-1;
+        // while (i<j){
+        //     int temp = arr[i];
+        //     arr[i] = arr[j];
+        //     arr[j] = temp;
+        //     i++;
+        //     j--;
+        // }
+        // i=0;
+        // j=n-1;
+        // while (i<j){
+        //     int temp = arr[i];
+        //     arr[i] = arr[j];
+        //     arr[j] = temp;
+        //     i++;
+        //     j--;
+        // }
         
-        for (int k = 0 ; k<n-1 ; k++){
-            if (arr[k]>arr[k+1]) return false;
-        }
-        return true;
+        // for (int k = 0 ; k<n-1 ; k++){
+        //     if (arr[k]>arr[k+1]) return false;
+        // }
+        // return true;
     }
 }
