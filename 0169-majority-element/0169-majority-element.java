7e27d87d4d49cae771d023b2aad1562ffeb02539
mohi010 <mohitgaming8595@gmail.com>
2026-06-30 22:59:08 +0530

    Time: 1 ms (99.89%), Space: 55.9 MB (6.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,36 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int cnt = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i =0 ; i<nums.length; i++){
-            if (map.containsKey(nums[i]) == true) map.put(nums[i], map.get(nums[i])+1);
+
+        //most optimal solution   
+        int cnt  = 0;
+        int ele = 0;
+        for (int i =0; i<nums.length; i++){
+            if (cnt==0) {
+                ele = nums[i];
+                cnt++;
+            }
+            else if (nums[i] == ele) cnt++;
             else {
-                map.put(nums[i], 1);
+                cnt--;
             }
         }
-        for (Integer key: map.keySet()){
-            if (map.get(key) > nums.length/2){
-                return key;
-            } 
-        }
-        return 5;
+        return ele;
+        
+
+
+        // int cnt = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i =0 ; i<nums.length; i++){
+        //     if (map.containsKey(nums[i]) == true) map.put(nums[i], map.get(nums[i])+1);
+        //     else {
+        //         map.put(nums[i], 1);
+        //     }
+        // }
+        // for (Integer key: map.keySet()){
+        //     if (map.get(key) > nums.length/2){
+        //         return key;
+        //     } 
+        // }
+        // return 5;
     }
 }
