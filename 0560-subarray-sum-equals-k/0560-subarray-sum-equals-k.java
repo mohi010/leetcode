37be2edf2e33c65e61ffeb5b6bac99bd9575050f
mohi010 <mohitgaming8595@gmail.com>
2026-07-09 20:47:13 +0530

    Time: 22 ms (96%), Space: 48.4 MB (93.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,7 +2,8 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
 
         // since there are positives and negatives i have to use hashmap for this particular problem
-
+        // i'm using hashmap in this problem with the pair value storing sum and the cnt of that sum if already occured i inc the cnt by 1 in the existing cnt, else i just normally put it into the hashmap with (sum,1)
+        
         HashMap<Integer, Integer> mpp = new HashMap<>();
         int sum=0;
         int cnt=0;
