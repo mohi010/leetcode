4093505fc13512c9acaa3c68bf738afa7a4668fc
mohi010 <mohitgaming8595@gmail.com>
2026-08-01 20:36:36 +0530

    Time: 2 ms (49.49%), Space: 52.8 MB (64.05%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,42 +10,68 @@
  * }
  */
 public class Solution {
+    public static int countNode(ListNode head){
+        ListNode temp = head;
+        int cnt  =0;
+        while(temp!=null){
+            cnt++;
+            temp = temp.next;
+        }
+        return cnt;
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        while(headA!=null && headB!=null){
-            if (map.containsKey(headA)) {
-                map.put(headA, map.get(headA)+1);
-            }
-            else {
-                map.put(headA, 1);
-            }
-            if (map.containsKey(headB)){
-            map.put(headB, map.get(headB)+1);
-            }
-            else {
-                map.put(headB, 1);
-            }
-            if(map.get(headA)==2) return headA;
-            if (map.get(headB)==2) return headB;
-            headB = headB.next;
+
+        int cntA = countNode(headA);
+        int cntB= countNode(headB);
+        if (cntA<cntB) return getIntersectionNode(headB, headA);
+        int k = Math.abs(cntA-cntB);
+        int i = 0;
+        while(i<k){
             headA = headA.next;
+            i++;
         }
         while(headA!=null){
-            if (map.containsKey(headA)) {
-                map.put(headA, map.get(headA)+1);
-            }
-            else map.put(headA, 1);
-            if (map.get(headA)==2) return headA;
-            headA= headA.next;
-        }
-        while(headB!=null){
-            if (map.containsKey(headB)) {
-                map.put(headB, map.get(headB)+1);
-            }
-            else map.put(headB, 1);
-            if (map.get(headB)==2) return headB;
-            headB= headB.next;
+            if(headA == headB) return headA;
+            headA = headA.next;
+            headB = headB.next;
         }
         return null;
+
+        // HashMap<ListNode, Integer> map = new HashMap<>();
+        // while(headA!=null && headB!=null){
+        //     if (map.containsKey(headA)) {
+        //         map.put(headA, map.get(headA)+1);
+        //     }
+        //     else {
+        //         map.put(headA, 1);
+        //     }
+        //     if (map.containsKey(headB)){
+        //     map.put(headB, map.get(headB)+1);
+        //     }
+        //     else {
+        //         map.put(headB, 1);
+        //     }
+        //     if(map.get(headA)==2) return headA;
+        //     if (map.get(headB)==2) return headB;
+        //     headB = headB.next;
+        //     headA = headA.next;
+        // }
+        // while(headA!=null){
+        //     if (map.containsKey(headA)) {
+        //         map.put(headA, map.get(headA)+1);
+        //     }
+        //     else map.put(headA, 1);
+        //     if (map.get(headA)==2) return headA;
+        //     headA= headA.next;
+        // }
+        // while(headB!=null){
+        //     if (map.containsKey(headB)) {
+        //         map.put(headB, map.get(headB)+1);
+        //     }
+        //     else map.put(headB, 1);
+        //     if (map.get(headB)==2) return headB;
+        //     headB= headB.next;
+        // }
+        // return null;
     }
 }
