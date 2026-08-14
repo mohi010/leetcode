c02700dec3d1aa8eb9298df746d30449694d0dbd
mohi010 <mohitgaming8595@gmail.com>
2026-08-14 22:58:53 +0530

    Time: 2 ms (19.16%), Space: 46.5 MB (91.88%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -25,33 +25,15 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root==null)  return new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        // List<Integer> subAns = new ArrayList<>();
         Queue<Pair> q = new LinkedList<>();
-        int curr  =0;
         q.add(new Pair(root, 1));
         while(q.size()>0){
             Pair temp = q.remove();
-            // if (curr== temp.second){
-            //     subAns.add(temp.first.val);  // ans.get(curr).add(temp.first.val);
-            // }
-            // else {
-            //     // ans.add(new ArrayList<>(subAns));
-            //     // subAns.clear();
-            //     curr = temp.second;
-            //     subAns.add(temp.first.val); // ans.get
-            // }
-            if (ans.size()!=temp.second){
-                // curr = temp.second;
-                ans.add(new ArrayList<>(List.of(temp.first.val)));
-
-            }
-            else{
-                ans.get(ans.size()-1).add(temp.first.val);
-            }
+            if (ans.size()!=temp.second) ans.add(new ArrayList<>(List.of(temp.first.val)));
+            else ans.get(ans.size()-1).add(temp.first.val);
             if (temp.first.left != null) q.add(new Pair(temp.first.left,temp.second +1 ));
             if (temp.first.right != null) q.add(new Pair(temp.first.right,temp.second +1 ));
         }
-        // if (!subAns.isEmpty()) ans.add(subAns);
         return ans;
     }
 }
